9019c859a9c4482e8a2a43c60733611906b09bea
Marcus Ritter <marcus.ritter@tu-darmstadt.de>
2023-05-24 17:40:30 +0200

    added scripts for converting relearn data and added measurements source file for extrap

--- relearn_scripts/relearn-125_execution_times/collect_execution_times.sh
+++ relearn_scripts/relearn-125_execution_times/collect_execution_times.sh
@@ -2,21 +2,49 @@ prefix=relearn
 
 P_VALUES="32 64 128 256 512"
 SIZE_VALUES="5000 6000 7000 8000 9000"
-THETA_VALUES="0.1 0.2 0.3 0.4 0.5" 
+THETA_VALUES="0.1"
+KERNELS=("  main()" "  Initialization" "  Simulation loop" "    Update electrical activity" "    Update #synaptic elements delta" "    Connectivity update" "      Update #synaptic elements + del synapses" "      Update local trees" "      Exchange branch nodes (w/ Allgather)" "      Insert branch nodes into global tree" "      Update global tree" "      Find target neurons (w/ RMA)" "      Empty remote nodes cache" "      Create synapses (w/ Alltoall)")
+FILENAME="relearn.txt"
+
+echo PARAMETER p >> $FILENAME
+echo PARAMETER n >> $FILENAME
+echo "" >> $FILENAME
 
 for p in $P_VALUES
 do
-    for size in $SIZE_VALUES
+    for n in $SIZE_VALUES
     do
-        LINE=""
-        
-        for theta in $THETA_VALUES
+        echo "POINTS ( $p $n )" >> $FILENAME
+    done
+done
+
+echo "" >> $FILENAME
+echo "METRIC runtime" >> $FILENAME
+echo "" >> $FILENAME
+
+for kernel in "${KERNELS[@]}"
+do
+    echo "REGION $kernel" >> $FILENAME
+
+    for p in $P_VALUES
+    do
+        for size in $SIZE_VALUES
         do
-            LINE="$LINE `grep "  main()" ${prefix}_p${p}_size${size}_theta${theta}*.out | head -n 1 | cut -f3 -d \| | cut -f2 -d " "`"
+            LINE="DATA "
+            
+            for theta in $THETA_VALUES
+            do
+                
+                LINE="$LINE `grep "$kernel" ${prefix}_p${p}_size${size}_theta${theta}*.out | head -n 1 | cut -f3 -d \| | cut -f2 -d " "`"
+                LINE="$LINE `grep -m2 "$kernel" ${prefix}_p${p}_size${size}_theta${theta}*.out | tail -n 1 | cut -f3 -d \| | cut -f2 -d " "`"
+                
+            done
+            
+            echo $LINE >> $FILENAME
+            
         done
-        
-        echo $LINE
     done
 
-    echo
+    echo "" >> $FILENAME
+
 done
