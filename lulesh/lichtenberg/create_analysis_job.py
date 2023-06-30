94c04aadf867daccd4ff1708194a9ad1f503016d
Marcus Ritter <ritter@cs.tu-darmstadt.de>
2023-07-01 01:17:11 +0200

    updated

--- lulesh/lichtenberg/create_analysis_job.py
+++ lulesh/lichtenberg/create_analysis_job.py
@@ -4,16 +4,16 @@ def main():
 
     counter = 14
 
-    while counter < 100:
+    while counter <= 100:
 
-        file = open("analysis_job_b"+str(counter)".sh","w")
+        file = open("analysis_job_b"+str(counter)+".sh","w")
 
         text = """#!/bin/bash
 
 #SBATCH -A project02089
 #SBATCH -t 00:29:00
 #SBATCH --mem-per-cpu=1800
-#SBATCH -n """+str(configs[i][0])+"""
+#SBATCH -n 1
 #SBATCH --exclusive
 #SBATCH -o out_b"""+str(counter)+""".out
 #SBATCH -e er_b"""+str(counter)+""".er
@@ -24,7 +24,7 @@ ml restore lulesh
 
 SECONDS=0;
 
-python .\case_study.py --cube E:\lulesh\ --processes 0 --parameters "p","s" --eval_point "1000","35" --filter 0 --budget """++str(counter)""" --normalization True
+python ../../case_study.py --cube ../../../data/lulesh/ --processes 0 --parameters "p","s" --eval_point "1000","35" --filter 0 --budget """+str(counter)+""" --normalization True
 
 echo $SECONDS"""
 
---
+++ lulesh/lichtenberg/submit_analysis_jobs.sh
@@ -0,0 +1,174 @@
+echo "submitting job with n=14"
+sbatch analysis_job_b14.sh
+echo "submitting job with n=15"
+sbatch analysis_job_b15.sh
+echo "submitting job with n=16"
+sbatch analysis_job_b16.sh
+echo "submitting job with n=17"
+sbatch analysis_job_b17.sh
+echo "submitting job with n=18"
+sbatch analysis_job_b18.sh
+echo "submitting job with n=19"
+sbatch analysis_job_b19.sh
+echo "submitting job with n=20"
+sbatch analysis_job_b20.sh
+echo "submitting job with n=21"
+sbatch analysis_job_b21.sh
+echo "submitting job with n=22"
+sbatch analysis_job_b22.sh
+echo "submitting job with n=23"
+sbatch analysis_job_b23.sh
+echo "submitting job with n=24"
+sbatch analysis_job_b24.sh
+echo "submitting job with n=25"
+sbatch analysis_job_b25.sh
+echo "submitting job with n=26"
+sbatch analysis_job_b26.sh
+echo "submitting job with n=27"
+sbatch analysis_job_b27.sh
+echo "submitting job with n=28"
+sbatch analysis_job_b28.sh
+echo "submitting job with n=29"
+sbatch analysis_job_b29.sh
+echo "submitting job with n=30"
+sbatch analysis_job_b30.sh
+echo "submitting job with n=31"
+sbatch analysis_job_b31.sh
+echo "submitting job with n=32"
+sbatch analysis_job_b32.sh
+echo "submitting job with n=33"
+sbatch analysis_job_b33.sh
+echo "submitting job with n=34"
+sbatch analysis_job_b34.sh
+echo "submitting job with n=35"
+sbatch analysis_job_b35.sh
+echo "submitting job with n=36"
+sbatch analysis_job_b36.sh
+echo "submitting job with n=37"
+sbatch analysis_job_b37.sh
+echo "submitting job with n=38"
+sbatch analysis_job_b38.sh
+echo "submitting job with n=39"
+sbatch analysis_job_b39.sh
+echo "submitting job with n=40"
+sbatch analysis_job_b40.sh
+echo "submitting job with n=41"
+sbatch analysis_job_b41.sh
+echo "submitting job with n=42"
+sbatch analysis_job_b42.sh
+echo "submitting job with n=43"
+sbatch analysis_job_b43.sh
+echo "submitting job with n=44"
+sbatch analysis_job_b44.sh
+echo "submitting job with n=45"
+sbatch analysis_job_b45.sh
+echo "submitting job with n=46"
+sbatch analysis_job_b46.sh
+echo "submitting job with n=47"
+sbatch analysis_job_b47.sh
+echo "submitting job with n=48"
+sbatch analysis_job_b48.sh
+echo "submitting job with n=49"
+sbatch analysis_job_b49.sh
+echo "submitting job with n=50"
+sbatch analysis_job_b50.sh
+echo "submitting job with n=51"
+sbatch analysis_job_b51.sh
+echo "submitting job with n=52"
+sbatch analysis_job_b52.sh
+echo "submitting job with n=53"
+sbatch analysis_job_b53.sh
+echo "submitting job with n=54"
+sbatch analysis_job_b54.sh
+echo "submitting job with n=55"
+sbatch analysis_job_b55.sh
+echo "submitting job with n=56"
+sbatch analysis_job_b56.sh
+echo "submitting job with n=57"
+sbatch analysis_job_b57.sh
+echo "submitting job with n=58"
+sbatch analysis_job_b58.sh
+echo "submitting job with n=59"
+sbatch analysis_job_b59.sh
+echo "submitting job with n=60"
+sbatch analysis_job_b60.sh
+echo "submitting job with n=61"
+sbatch analysis_job_b61.sh
+echo "submitting job with n=62"
+sbatch analysis_job_b62.sh
+echo "submitting job with n=63"
+sbatch analysis_job_b63.sh
+echo "submitting job with n=64"
+sbatch analysis_job_b64.sh
+echo "submitting job with n=65"
+sbatch analysis_job_b65.sh
+echo "submitting job with n=66"
+sbatch analysis_job_b66.sh
+echo "submitting job with n=67"
+sbatch analysis_job_b67.sh
+echo "submitting job with n=68"
+sbatch analysis_job_b68.sh
+echo "submitting job with n=69"
+sbatch analysis_job_b69.sh
+echo "submitting job with n=70"
+sbatch analysis_job_b70.sh
+echo "submitting job with n=71"
+sbatch analysis_job_b71.sh
+echo "submitting job with n=72"
+sbatch analysis_job_b72.sh
+echo "submitting job with n=73"
+sbatch analysis_job_b73.sh
+echo "submitting job with n=74"
+sbatch analysis_job_b74.sh
+echo "submitting job with n=75"
+sbatch analysis_job_b75.sh
+echo "submitting job with n=76"
+sbatch analysis_job_b76.sh
+echo "submitting job with n=77"
+sbatch analysis_job_b77.sh
+echo "submitting job with n=78"
+sbatch analysis_job_b78.sh
+echo "submitting job with n=79"
+sbatch analysis_job_b79.sh
+echo "submitting job with n=80"
+sbatch analysis_job_b80.sh
+echo "submitting job with n=81"
+sbatch analysis_job_b81.sh
+echo "submitting job with n=82"
+sbatch analysis_job_b82.sh
+echo "submitting job with n=83"
+sbatch analysis_job_b83.sh
+echo "submitting job with n=84"
+sbatch analysis_job_b84.sh
+echo "submitting job with n=85"
+sbatch analysis_job_b85.sh
+echo "submitting job with n=86"
+sbatch analysis_job_b86.sh
+echo "submitting job with n=87"
+sbatch analysis_job_b87.sh
+echo "submitting job with n=88"
+sbatch analysis_job_b88.sh
+echo "submitting job with n=89"
+sbatch analysis_job_b89.sh
+echo "submitting job with n=90"
+sbatch analysis_job_b90.sh
+echo "submitting job with n=91"
+sbatch analysis_job_b91.sh
+echo "submitting job with n=92"
+sbatch analysis_job_b92.sh
+echo "submitting job with n=93"
+sbatch analysis_job_b93.sh
+echo "submitting job with n=94"
+sbatch analysis_job_b94.sh
+echo "submitting job with n=95"
+sbatch analysis_job_b95.sh
+echo "submitting job with n=96"
+sbatch analysis_job_b96.sh
+echo "submitting job with n=97"
+sbatch analysis_job_b97.sh
+echo "submitting job with n=98"
+sbatch analysis_job_b98.sh
+echo "submitting job with n=99"
+sbatch analysis_job_b99.sh
+echo "submitting job with n=100"
+sbatch analysis_job_b100.sh
