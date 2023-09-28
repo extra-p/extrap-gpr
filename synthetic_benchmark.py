fixed bugs in hybrid strategy, corrected add point calculation, nicer plots

synthetic_benchmark.py
@@ -436,6 +436,7 @@ class SyntheticBenchmark():
         
         if self.grid_search == 2 or self.grid_search == 3:
             measurements_gpr = copy.deepcopy(experiment.measurements)
+            measurements_hybrid = copy.deepcopy(experiment.measurements)
         base_values = 1
 
         if len(experiment.parameters) == 2:
@@ -477,6 +478,12 @@ class SyntheticBenchmark():
                     
                     if self.grid_search == 2 or self.grid_search == 3:
                         for x in measurements_gpr[(Callpath("main"), Metric("runtime"))]:
+                            if x.coordinate == cord:
+                                temp = x.values
+                                for i in range(base_values):
+                                    temp = np.delete(temp, 0)
+                                x.values = temp
+                        for x in measurements_hybrid[(Callpath("main"), Metric("runtime"))]:
                             if x.coordinate == cord:
                                 temp = x.values
                                 for i in range(base_values):
@@ -531,6 +538,12 @@ class SyntheticBenchmark():
                     
                     if self.grid_search == 2 or self.grid_search == 3:
                         for x in measurements_gpr[(Callpath("main"), Metric("runtime"))]:
+                            if x.coordinate == cord:
+                                temp = x.values
+                                for i in range(base_values):
+                                    temp = np.delete(temp, 0)
+                                x.values = temp
+                        for x in measurements_hybrid[(Callpath("main"), Metric("runtime"))]:
                             if x.coordinate == cord:
                                 temp = x.values
                                 for i in range(base_values):
@@ -921,7 +934,7 @@ class SyntheticBenchmark():
         base_point_cost = base_point_cost / (total_cost / 100)
         #print("base_point_cost %:",base_point_cost)
 
-        added_points_generic = 0
+        added_points_generic = len(selected_points) * (self.nr_repetitions-1)
 
         #print("len selected_points:",len(selected_points))
         
@@ -953,17 +966,6 @@ class SyntheticBenchmark():
         current_cost_percent = current_cost / (total_cost / 100)
         #print("current_cost_percent:",current_cost_percent)
         #print("self.budget:",self.budget)
-
-        # check if the cost of the base points is higher than the allowed budget
-        #print("current_cost_percent <= self.budget 11:", current_cost_percent, self.budget)
-        #if current_cost_percent <= self.budget:
-
-            # start by adding for each additional dimension one additional point
-            #for o in range(self.nr_parameters-1):
-
-                #remaining_points_generic, selected_points_generic, _ = add_additional_point_generic(remaining_points_generic, selected_points_generic)
-                # increment counter value, because a new measurement point was added
-                #added_points_generic += 1
 
         if self.mode == "budget":
 
@@ -999,7 +1001,8 @@ class SyntheticBenchmark():
                         #print("point_map_generic:",point_map_generic)
 
                         # increment counter value, because a new measurement point was added
-                        added_points_generic += 1
+                        #added_points_generic += 1
+                        added_points_generic += self.nr_repetitions
 
                         # create new model
                         experiment_generic_base = create_experiment(selected_coord_list_new, experiment, len(experiment.parameters), parameters, 0, 0)
@@ -1175,9 +1178,11 @@ class SyntheticBenchmark():
 
         # add additional measurement points until break criteria is met
         add_points_gpr = 0
+        
         budget_core_hours = self.budget * (total_cost / 100)
         
         if self.grid_search == 1 or self.grid_search == 4:
+            add_points_gpr = len(selected_points) * (self.nr_repetitions-1)
             remaining_points_gpr = copy.deepcopy(remaining_points)
             # entails all measurement points and their values
             measurements_gpr = copy.deepcopy(experiment.measurements)
@@ -1200,14 +1205,6 @@ class SyntheticBenchmark():
             experiment_gpr_base = self.create_experiment_base(selected_points_gpr, experiment, len(experiment.parameters), parameters, 0, 0, base_values)
         else:
             experiment_gpr_base = create_experiment(selected_points_gpr, experiment, len(experiment.parameters), parameters, 0, 0)
-        
-        """x = []
-        for i in range(len(experiment_gpr_base.measurements[(Callpath("main"), Metric("runtime"))])):
-            x.append((experiment_gpr_base.measurements[(Callpath("main"), Metric("runtime"))][i].coordinate, experiment_gpr_base.measurements[(Callpath("main"), Metric("runtime"))][i].values))
-        
-        for l in x:
-            print(l)"""
-                
         
         if self.mode == "budget":
 
@@ -1264,8 +1261,6 @@ class SyntheticBenchmark():
                     #print("DEBUG3 remaining_points_gpr:",remaining_points_gpr[fitting_measurements[i]][0])
                     
                     # term_1 is cost(t)^2
-                    #TODO: first option is actually incorrect but seems to improve gpr results...
-                    #term_1 = math.pow(np.sum(remaining_points_gpr[fitting_measurements[i]]), 2)
                     term_1 = math.pow(remaining_points_gpr[fitting_measurements[i]][0], 2)
                     # predict variance of input vector x with the gaussian process
                     x = [x]
@@ -1440,11 +1435,15 @@ class SyntheticBenchmark():
         # add additional measurement points until break criteria is met
         add_points_hybrid = 0
         budget_core_hours = self.budget * (total_cost / 100)
-
-        #remaining_points_hybrid = copy.deepcopy(remaining_points_min)
-        remaining_points_hybrid = copy.deepcopy(remaining_points)
+        
+        if self.grid_search == 1 or self.grid_search == 4:
+            add_points_hybrid = len(selected_points) * (self.nr_repetitions-1)
+            remaining_points_hybrid = copy.deepcopy(remaining_points)
+            # entails all measurement points and their values
+            measurements_hybrid = copy.deepcopy(experiment.measurements)
+        elif self.grid_search == 2 or self.grid_search == 3:
+            remaining_points_hybrid = copy.deepcopy(remaining_points_min)
         selected_points_hybrid = copy.deepcopy(selected_points)
-        measurements_hybrid = copy.deepcopy(experiment.measurements)
 
         # add all of the selected measurement points to the gaussian process
         # as training data and train it for these points
@@ -1495,13 +1494,17 @@ class SyntheticBenchmark():
                 # determine the switching point between gpr and hybrid strategy
                 swtiching_point = 0
                 if len(experiment.parameters) == 2:
-                    swtiching_point = 13
+                    if self.grid_search == 1 or self.grid_search == 4:
+                        swtiching_point = 41
+                    elif self.grid_search == 2 or self.grid_search == 3:
+                        swtiching_point = 14
+                #TODO: swtiching_point needs to be recalculated for 3 and 4 parameter experiments!!!
                 elif len(experiment.parameters) == 3:
                     swtiching_point = 18
                 elif len(experiment.parameters) == 4:
                     swtiching_point = 23
                 else:
-                    swtiching_point = 13
+                    swtiching_point = 41
 
                 best_index = -1
                 
@@ -1523,7 +1526,8 @@ class SyntheticBenchmark():
                                 x.append(parameter_values[j])
                         
                         # term_1 is cost(t)^2
-                        term_1 = math.pow(np.sum(remaining_points_hybrid[fitting_measurements[i]]), 2)
+                        #term_1 = math.pow(np.sum(remaining_points_hybrid[fitting_measurements[i]]), 2)
+                        term_1 = math.pow(remaining_points_hybrid[fitting_measurements[i]][0], 2)
                         # predict variance of input vector x with the gaussian process
                         x = [x]
                         _, y_cov = gaussian_process_hybrid.predict(x, return_cov=True)
@@ -1531,7 +1535,19 @@ class SyntheticBenchmark():
                         # term_2 is gp_cov(t,t)^2
                         term_2 = math.pow(y_cov, 2)
                         # rated is h(t)
-                        rated = term_1 / term_2
+                        
+                        if self.grid_search == 3 or self.grid_search == 4:
+                            rep = 1
+                            for j in range(len(measurements_hybrid[(Callpath("main"), Metric("runtime"))])):
+                                if measurements_hybrid[(Callpath("main"), Metric("runtime"))][j].coordinate == fitting_measurements[i]:
+                                    rep = (self.nr_repetitions - len(measurements_hybrid[(Callpath("main"), Metric("runtime"))][j].values)) + 1
+                                    break
+                            rep_func = 2**((1/2)*rep-(1/2))
+                            noise_func = -math.tanh((1/4)*mean_noise-2.5)
+                            cost_multiplier = rep_func + noise_func
+                            rated = (term_1 * cost_multiplier) / term_2
+                        else:
+                            rated = term_1 / term_2
 
                         if rated <= best_rated:
                             best_rated = rated
@@ -1543,8 +1559,9 @@ class SyntheticBenchmark():
                     for i in range(len(fitting_measurements)):
                         
                         # get the cost of the measurement point
-                        cost = np.sum(remaining_points_hybrid[fitting_measurements[i]])
-                    
+                        #cost = np.sum(remaining_points_hybrid[fitting_measurements[i]])
+                        cost = remaining_points_hybrid[fitting_measurements[i]][0]
+
                         if cost < lowest_cost:
                             lowest_cost = cost
                             best_index = i
@@ -2002,7 +2019,7 @@ class SyntheticBenchmark():
 
         add_points = {
             "Base points": np.array([min_points, min_points, min_points, min_points]),
-            "Additional points": np.array([len_coordinates-min_points, mean_add_points_generic, mean_add_points_gpr, mean_add_points_hybrid]),
+            "Additional points": np.array([len_coordinates*self.nr_repetitions-min_points, mean_add_points_generic, mean_add_points_gpr, mean_add_points_hybrid]),
         }
 
         # plot the analysis result for the additional measurement point numbers

synthetic_evaluation/2_parameter/1_noise/plot.py
@@ -1,5 +1,6 @@
 import json
 import os
+import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 import argparse
@@ -19,38 +20,29 @@ def find_files(folder_path):
     return file_list
 
 def plot_accuracy(x_values, y_values_list, labels, bucket):
-    ls=['-','--',':','-']
-    lw = [2,3,5,2]
+    ls=["-",'dotted','--',':','-']
+    lw = [1,2,2,5,2]
+    #plt.rc('text', usetex=True)
+    #plt.rcParams["font.size"] = 9.5
     plt.xscale("symlog")
-    #plt.xlim(, 100)
+    plt.xlim(0.1, 120)
     style_counter = 0
-    colors=["black", "red", "green", "yellow"]
-    
-    #for i in range(len(y_values_list)):
-        #print(len(y_values_list[i]))
-    
+    zorders=[5,4,3,2,1]
+    colors=["gray", "blue", "red", "orange", "yellow"]
     for y_values, label, color in zip(y_values_list, labels, colors):
         if style_counter == 0:
-            plt.plot(100, y_values[len(y_values)-1], label=label, marker="D", linestyle = 'None',)
+            plt.scatter(100, y_values[len(y_values)-1], label=label, marker="D", linestyle = 'None', zorder=10, edgecolor="black", facecolor=colors[style_counter])
         else:
-            plt.plot(x_values, y_values, label=label, linestyle=ls[style_counter], linewidth=lw[style_counter], alpha=0.7)
-            if style_counter == 2:
-                #print(len(y_values), len(y_values_list[1]))
-                #print(y_values, y_values_list[1])
-                plt.fill_between(x_values, y_values_list[1], y_values, color="green", where=np.array(y_values) > np.array(y_values_list[1]), alpha=0.4, label='gpr better than generic')
-                plt.fill_between(x_values, y_values, y_values_list[1], color="red", where=np.array(y_values) < np.array(y_values_list[1]), alpha=0.4, label='gpr worse than generic')
-            #if style_counter == 3:
-            #    plt.fill_between(x_values, y_values_list[1], y_values, facecolor="red", alpha=0.4, label='diff hybrid generic')
-        
+            plt.plot(x_values, y_values, label=label, linestyle=ls[style_counter], linewidth=lw[style_counter], alpha=0.7, color=colors[style_counter], zorder=zorders[style_counter])
         style_counter += 1
-    
-    #plt.fill_between(x_values, y_values[1], y_values[2], where=y_values[2]>y_values[1], color="red")
-    
+    plt.fill_between(x_values, y_values_list[1], y_values_list[2], color="green", where=np.array(y_values_list[2]) > np.array(y_values_list[1]), alpha=0.4, label='GPR better than CPF', hatch="x")
+    plt.fill_between(x_values, y_values_list[1], y_values_list[2], color="red", where=np.array(y_values_list[1]) > np.array(y_values_list[2]), alpha=0.4, label='GPR worse than CPF', hatch="+", zorder=5)
+    plt.grid(alpha=0.3)
     plt.yticks(np.arange(0, 100, 10))
     plt.xticks([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5,6,7,8,8,9,10,20,30,40,50,60,70,80,90,100])
-    plt.xlabel('Allowed modeling budget [%]')
+    plt.xlabel('Allowed modeling budget $b$ [%]')
     plt.ylabel('Models in '+str(bucket)+'% accuracy bucket [%]')
-    plt.legend()
+    plt.legend(loc="lower right")
     plt.savefig('accuracy_'+str(bucket)+'.png')
     plt.show()
     plt.close()
@@ -94,30 +86,34 @@ def plot_cost(x_values, y_values_list, labels, bucket):
     plt.close()
 
 def plot_selected_points(x_values, y_values_list, labels, bucket):
-    ls=['-','--',':','-']
-    lw = [2,3,5,2]
+    ls=["-",'dotted','--',':','dashdot']
+    lw = [1,2,2,5,2]
+    #plt.rc('text', usetex=True)
+    #plt.rcParams["font.size"] = 9.5
+    #plt.xlim(0.1, 120)
     style_counter = 0
+    zorders=[5,4,3,2,1]
+    colors=["gray", "blue", "red", "orange", "dimgray"]
+    plt.xscale("symlog")
     for y_values, label in zip(y_values_list, labels):
-        plt.plot(x_values, y_values, label=label, linestyle=ls[style_counter], linewidth=lw[style_counter], alpha=0.7)
+        if style_counter == 0:
+            plt.scatter(100, y_values[len(y_values)-1], label=label, marker="D", linestyle = 'None', zorder=10, edgecolor="black", facecolor=colors[style_counter])
+        else:
+            plt.plot(x_values, y_values, label=label, linestyle=ls[style_counter], linewidth=lw[style_counter], alpha=0.7, color=colors[style_counter], zorder=zorders[style_counter])
         style_counter += 1
-    plt.ylabel('Mean Number of Points Used for Modelnig')
-    plt.xlabel('Allowed Modeling Budget [%]')
-    temp = list(plt.yticks()[0])
-    temp.append(9)
-    temp.append(25)
-    plt.yticks(temp)
-    temp = list(plt.xticks()[0])
-    temp.append(1)
-    plt.xticks(temp)
-    plt.xlim(1,100)
-    #plt.ylim(8.5,25)
-    plt.legend(loc='center right')
+    plt.ylabel('Mean number of points used for modelnig $\\bar{p}$')
+    plt.xlabel('Allowed modeling budget $b$ [%]')
+    plt.grid(alpha=0.3)
+    plt.yticks(np.arange(0, 110, 10))
+    plt.xticks([1,10,20,30,40,50,60,70,80,90,100])
+    plt.xlim(0,120)
+    plt.legend(loc='upper left')
     plt.savefig('additional_points.png')
     plt.show()
     plt.close()
 
 def main():
-
+    
     # Create the argument parser
     parser = argparse.ArgumentParser(description='Plotting tool for analysis.')
 
@@ -158,6 +154,8 @@ def main():
 
     base_point_costs = []
     base_points = []
+    
+    all_points = []
 
     files = natsorted(files)
     #print("DEBUG:",len(files))
@@ -179,6 +177,8 @@ def main():
         points_generic.append(json_data["mean_add_points_generic"]+json_data["min_points"])
         points_gpr.append(json_data["mean_add_points_gpr"]+json_data["min_points"])
         points_hybrid.append(json_data["mean_add_points_hybrid"]+json_data["min_points"])
+        
+        all_points.append(100)
 
         base_point_costs.append(json_data["base_point_cost"])
 
@@ -201,6 +201,7 @@ def main():
         base_point_costs
     ]
     y_values_list3 = [
+        all_points,
         points_generic,
         points_gpr,
         points_hybrid,
@@ -217,9 +218,9 @@ def main():
     #print(np.max(gpr_costs))
     #print(np.max(hybrid_costs))
 
-    labels = ['Full matrix', 'Generic strategy', 'GPR strategy', 'Hybrid strategy']
+    labels = ['Full matrix', 'CPF strategy', 'GPR strategy', 'Hybrid strategy']
     labels2 = ['CPF strategy', 'GPR strategy', 'Hybrid strategy', 'Min. modeling requirement $\\bar{b}_{min}$']
-    labels3 = ['Generic', 'GPR', 'Hybrid', 'base points']
+    labels3 = ['All available points', 'CPF strategy', 'GPR strategy', 'Hybrid strategy', 'Min. points required $\\bar{p}_{min}$']
 
     plot_accuracy(budget_values, y_values_list, labels, bucket)
     plot_cost(budget_values, y_values_list2, labels2, bucket)