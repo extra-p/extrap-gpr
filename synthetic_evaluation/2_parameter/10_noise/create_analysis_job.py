0ccadcfd1ed79c6c3aa9b258a29b7a5b00bdc8ec
Marcus Ritter <ritter@cs.tu-darmstadt.de>
2023-10-04 18:44:52 +0200

    updated job script and set final config for evaluation

--- synthetic_evaluation/2_parameter/10_noise/create_analysis_job.py
+++ synthetic_evaluation/2_parameter/10_noise/create_analysis_job.py
@@ -25,7 +25,7 @@ ml restore lulesh
 
 SECONDS=0;
 
-python ../../../synthetic_evaluation.py --nr-parameters 2 --nr-functions 1000 --nr-repetitions 4 --noise 10 --mode budget --budget """+str(counter)+""" --normalization True --plot True --grid-search 3 --base-values 2 --hybrid-switch 10
+python ../../../synthetic_evaluation.py --nr-parameters 2 --nr-functions 10000 --nr-repetitions 4 --noise 10 --mode budget --budget """+str(counter)+""" --normalization True --plot True --grid-search 4 --base-values 2 --hybrid-switch 20
 
 echo $SECONDS"""
 
