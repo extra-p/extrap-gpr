1d8ac60301006d4ddfd2d1089be600ff6950d965
Marcus Ritter <ritter@cs.tu-darmstadt.de>
2023-11-06 19:52:13 +0100

    updated lulesh config to improve results

--- lulesh/lichtenberg/create_analysis_job.py
+++ lulesh/lichtenberg/create_analysis_job.py
@@ -5,7 +5,7 @@ import math
 
 def main():
 
-    counter = 9.0
+    counter = 13.0
 
     while counter <= 100.0:
         
@@ -16,7 +16,7 @@ def main():
 
 #SBATCH -A project02089
 #SBATCH -t 00:10:00
-#SBATCH --mem-per-cpu=1800
+#SBATCH --mem-per-cpu=3800
 #SBATCH -n 1
 #SBATCH --exclusive
 #SBATCH -o out_b"""+str(counter_string)+""".out
@@ -28,7 +28,7 @@ ml restore lulesh
 
 SECONDS=0;
 
-python ../../case_study.py --cube /work/scratch/mr52jiti/data/lulesh/ --processes 0 --parameters "p","s" --eval_point "1000","35" --filter 1 --budget """+str(counter)+""" --plot True --normalization True --grid-search 3 --base-values 2 --hybrid-switch 20 --repetition 5
+python ../../case_study.py --cube /work/scratch/mr52jiti/data/lulesh/ --processes 0 --parameters "p","s" --eval_point "1000","35" --filter 0 --budget """+str(counter)+""" --plot True --normalization True --grid-search 3 --base-values 2 --hybrid-switch 20 --repetition 5
 
 echo $SECONDS"""
 
