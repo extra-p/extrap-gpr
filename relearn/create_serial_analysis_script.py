8b6278abe328aa4cba14db8d2fe7551833fa0444
Marcus Ritter <19738708+marcusritter1@users.noreply.github.com>
2023-11-10 00:05:15 -0800

    Update create_serial_analysis_script.py

--- relearn/create_serial_analysis_script.py
+++ relearn/create_serial_analysis_script.py
@@ -4,14 +4,14 @@ import math
 
 def main():
 
-    counter = 9.0
+    counter = 13.0
 
     text = ""
     while counter <= 100.0:
 
         counter_string = "{:0.1f}".format(counter)
 
-        text += """python3 ../case_study.py --text relearn_data.txt --processes 0 --parameters "p","n" --eval_point "512","9000" --filter 1 --budget """+str(counter)+""" --plot True --normalization True --grid-search 3 --base-values 1 --hybrid-switch 20 --repetition 2\n"""
+        text += """python3 ../case_study.py --text relearn_data.txt --processes 0 --parameters "p","n" --eval_point "512","9000" --filter 0 --budget """+str(counter)+""" --plot True --normalization True --grid-search 3 --base-values 1 --hybrid-switch 20 --repetition 2\n"""
 
         if counter < 0.9:
             counter += 0.1
