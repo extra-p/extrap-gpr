15062a75ead2637e52ef4534e61988387bb6fbac
Marcus Ritter <19738708+marcusritter1@users.noreply.github.com>
2023-11-10 00:09:32 -0800

    Create docker_run_analysis.sh

---
+++ synthetic_evaluation/2_parameter/1_noise/docker_run_analysis.sh
@@ -0,0 +1,19 @@
+python3 create_serial_analysis_script.py
+chmod +x run_serial_analysis.sh
+./run_serial_analysis.sh
+mkdir accuracy_plots analysis_results cost_plots point_plots
+mv result.budget.* analysis_results/
+mv accuracy_b* accuracy_plots/
+mv cost_b* cost_plots/
+mv additional_points_b* point_plots/
+python3 plot.py --bucket 5 --reps 2
+python3 plot.py --bucket 10 --reps 2
+python3 plot.py --bucket 15 --reps 2
+python3 plot.py --bucket 20 --reps 2
+mkdir results
+mv accuracy_*.png results/
+mv additional_points.png results/
+mv cost.png results/
+./archive.sh all
+python3 single_plot.py --path all/analysis_results/ --name results_all --reps 2 --min 9 --filter 1
+python3 budget_usage_plot.py --path all/analysis_results/ --name budget_usage_all --reps 2 --min 9
