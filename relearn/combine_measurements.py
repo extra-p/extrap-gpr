40621eae993ed90093e235d7a381c32955a6f869
Marcus Ritter <ritter@cs.tu-darmstadt.de>
2025-06-11 10:19:03 +0200

    added extended results for relearn

---
+++ relearn/combine_measurements.py
@@ -0,0 +1,25 @@
+def main():
+
+    text = ""
+
+    file = open("combine_measurements.sh","w")
+
+    counter = 0.1
+
+    while counter <= 100.0:
+
+        counter_string = "{:0.1f}".format(counter)
+        text += f"python3 read_measurements.py extended_analysis/analysis_results/result.budget.{counter_string}.json all/analysis_results/result.budget.{counter_string}.json result.budget.{counter_string}.json\n"
+
+        if counter < 0.9:
+            counter += 0.1
+        else:
+            counter += 1.0
+    
+    file.write(text)
+
+    file.close()
+
+
+if __name__ == '__main__':
+    main()
